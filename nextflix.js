c2dd24c9dcc37a3e6440322cacb23a0098710527
Tamir Bahar <tamir.bahar@gmail.com>
2024-03-05 17:45:00 +0200

    Added handling for the center play button

--- nextflix.js
+++ nextflix.js
@@ -14,3 +14,23 @@ document.addEventListener("keyup", (event) => {
                      document.querySelector('[data-uia="control-next"]');
     nextButton.click();
 });
+
+
+document.addEventListener("keyup", (event) => {
+    // Make sure we pressed `Space`
+    if (event.code != "Space" || event.shiftKey || event.altKey || event.ctrlKey || event.metaKey) {
+        return;
+    }
+
+    // It's important to make sure we only click the weird center button, not the regular one.
+    // Otherwise, we might mess with the functionality of clicking "Space" in general.
+    // But since we specifically find the center button, we're ok.
+
+    // Find the center button by looking for a "Play" button that is not a play-pause button.
+    let playButton = document.querySelector("[aria-label='Play']:not([data-uia='control-play-pause-play'])");
+    playButton?.click();
+
+    // Alternatively, we can look for the center play button specifically using:
+    // document.querySelector("[aria-label='Play'][data-uia='player-blocked-play']")
+    // But I prefer the above as I don't have to worry about multiple types of center buttons.
+});
